6b68b52973c2140502becf6061a0502d0c36efb6
breezy-karma-0220 <lh62594@yahoo.com>
2018-11-15 01:44:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -40,15 +40,17 @@ function total() {
 }
 
 function removeFromCart(item) {
-  var itemInCart = false
-  for(let i = 0; i < cart.length; i++) {
-    if(cart[i].hasOwnProperty(item)) {
-      itemInCart = true
+  var items = []
+  var cartLength = cart.length
+  for(var i = 0; i < cart.length; i++) {
+    if(cart[i].itemName === item) {
       cart.splice(i,1)
+      return cart
+    } else {
+      items.push(cart[i].itemName)
     }
   }
-  if(!itemInCart){
+  if(items.length === cartLength) {
     return "That item is not in your cart."
   }
-  return cart
 }
